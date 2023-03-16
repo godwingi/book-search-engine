dfed73b4fce31bfad17cb3065da8e22d7a4bbe61
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-03-16 19:47:18 -0400

    mutations added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,8 +5,23 @@ const resolvers = {
     books: async () => {
       return await Book.find({});
     },
-    users: async () => {
-      return await User.find({});
+    me: async (parent, args) => {
+      return await User.findById(args.id);
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      return await User.create({ username, email, password });
+    },
+    login: async (parent, { email, password }) => {
+      return await Auth.findOrUpdateOrCreate({ email, password });
+    },
+    saveBook: async (parent, { input: BookInput }) => {
+      return await User.create({ input: BookInput });
+    },
+    removeBook: async (parent, bookId) => {
+      return await User.removeById(bookId);
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,48 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-const typeDefs = gql `
-    type Book {
-        _id: ID!
-        authors: String!
-        description: String!
-        bookId: String!
-        image: String!
-        link: String!
-        title: String!
-    }
+const typeDefs = gql`
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
-    type User {
-        _id: ID!
-        username: String!
-        savedBooks: Book!
-    }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Query{
-        books: [Book]
-        users: [User]
-    }
+  type Auth {
+    token: String!
+    user: User
+  }
+
+  type Query {
+    me: User
+    books: [Book]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
